d3e78b0da768c8d807efa83e28e56767eb478d5c
sandro <sandro.blatter@hotmail.com>
2025-02-28 16:53:25 +0100

    init

---
+++ patient-checkin/src/main/java/com/eventdriven/healthcare/patientcheckin/PatientCheckinApplication.java
@@ -0,0 +1,13 @@
+package com.eventdriven.healthcare.patientcheckin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientCheckinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientCheckinApplication.class, args);
+    }
+
+}
