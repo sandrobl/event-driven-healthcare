be7333d77330df70befc741acc178f27f442ddb2
sandro <sandro.blatter@hotmail.com>
2025-03-04 11:26:38 +0100

    feat: add Patient model and update Kafka integration for patient events

--- patient-checkin/src/main/java/com/eventdriven/healthcare/patientcheckin/controllers/RestController.java
+++ patient-checkin/src/main/java/com/eventdriven/healthcare/patientcheckin/controllers/RestController.java
@@ -1,80 +1,30 @@
 package com.eventdriven.healthcare.patientcheckin.controllers;
 
-import com.eventdriven.healthcare.patientcheckin.model.Gaze;
+import com.eventdriven.healthcare.patientcheckin.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.eventdriven.healthcare.patientcheckin.service.ProducerService;
+import org.springframework.web.server.ResponseStatusException;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = "/kafka")
 public class RestController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    Boolean singleEyeTrackingThreadStart = false;
-    Boolean singleClickTrackingThreadStart = false;
-
     @Autowired
-    private ProducerService<Gaze> producerService;
+    private ProducerService<Patient> producerService;
 
-    @GetMapping(value = "/eyeTracking")
-    public String eyeTrackingCall(@RequestParam("action") String action) {
-        String output = "";
-
-        if(action.equals("start") & !singleEyeTrackingThreadStart) {
-            // starting eye-tracking in a new thread if not already started
-            Thread eyeTrackingThread =  new Thread(() -> {
-                producerService.startEyeTracker();
-            });
-            eyeTrackingThread.start();
-            singleEyeTrackingThreadStart = true;
-            output = "Eye-tracker successfully started!";
+    @GetMapping(value = "/patientInformationRequest")
+    public String patientInformationRequest(@RequestParam("patientId") int patientId) {
+        logger.info("Patient information request for patient ID :: " + patientId);
+        if(patientId > 0) {
+            producerService.sendPatientInformationRequest(patientId);
+            return "Patient information request sent for patient ID :: " + patientId;
+        }else{
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid patient ID");
         }
-        else if(singleEyeTrackingThreadStart) {
-            output = "Eye-tracker already running";
-        }
-        else {
-            output = "Unknown action";
-        }
-
-        return output;
     }
-
-    @GetMapping(value = "/eyeTrackingStop")
-    public String eyeTrackingStop() {
-        logger.info("Stopping eye tracking");
-        return "Eye tracking stopped";
-    }
-
-    @GetMapping(value = "/clickTracking")
-    public String clickTrackingCall(@RequestParam("action") String action) {
-        String output = "";
-
-        logger.info("action: " + action);
-
-        if(action.equals("start") & !singleClickTrackingThreadStart) {
-            // starting clicks tracking in a new thread if not already started
-            Thread clickTrackingThread =  new Thread(() -> {
-                producerService.startClicksTracker();
-            });
-            clickTrackingThread.start();
-            singleClickTrackingThreadStart = true;
-            output = "Started recording user clicks!";
-        }
-        else if(singleClickTrackingThreadStart) {
-            output = "the recording of users clicks is already running";
-        }
-        else {
-            output = "Unknown action";
-        }
-
-        return output;
-    }
-
-
-
-
-
-
 }
---
+++ patient-checkin/src/main/java/com/eventdriven/healthcare/patientcheckin/model/Patient.java
@@ -0,0 +1,45 @@
+package com.eventdriven.healthcare.patientcheckin.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class Patient implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    int patientID;
+    String name;
+    String firstname;
+    float height;
+    float weight;
+    float bloodGlucose;
+
+    public Patient() {}
+    public Patient(int patientID, String name, String firstname, float height, float weight, float bloodGlucose) {
+        this.patientID = patientID;
+        this.name = name;
+        this.firstname = firstname;
+        this.height = height;
+        this.weight = weight;
+        this.bloodGlucose = bloodGlucose;
+    }
+
+    public Patient(int patientID){
+        this.patientID = patientID;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "patientID: " + patientID +
+                ", name: " + name +
+                ", firstname: " + firstname +
+                ", height: " + height +
+                ", weight: " + weight +
+                ", bloodGlucose: " + bloodGlucose;
+    }
+}
